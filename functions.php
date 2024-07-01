f67b56710ee99d53170aa73d1d909f85a80074db
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2024-07-01 20:34:57 +0330

    Update functions.php

--- functions.php
+++ functions.php
@@ -426,3 +426,9 @@ function ping($host, $port, $timeout) {
     $tA = microtime(true);
     return round((($tA - $tB) * 1000), 0) . " ms";
 }
+
+function generateHiddifyTags () {
+    $profileTitle = base64encode("HiN Outliner 🫧");
+    return "#profile-title: {$profileTitle}\n#profile-update-interval: 1\n#subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=2546249531\n#support-url: https://hingroup.t.me\n#profile-web-page-url: https://Here_is_Nowhere.t.me
+";
+}
